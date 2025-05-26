967911b93cbd7bfe135c983298616bb5fd82b5a2
DoutorK <giovannylucas28@gmail.com>
2025-05-26 13:16:28 -0300

    feat: adiciona instrumentação do Prometheus ao aplicativo FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from book_api import router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 app.include_router(router)
+
+Instrumentator().instrument(app).expose(app)
